836b460e837918f620e774e2c34db60f2ac9ee80
Denis Yavorsky <svirgibs@gmail.com>
2024-07-17 23:10:16 +0300

    irregular_mountains.py

--- context_tasks/irregular_mountains/irregular_mountains.py
+++ context_tasks/irregular_mountains/irregular_mountains.py
@@ -1,12 +1,34 @@
-def main():
-    pass
+def valid_mountain_array(height_array):
+    height_increase = True
+
+    if len(height_array) < 3:
+        return False
+
+    if height_array[-1] > height_array[-2] or height_array[0] > height_array[-1]:
+        return False
+
+    for index, value in enumerate(height_array):
+        if index == 0:
+            continue
+        if value > height_array[index - 1] and height_increase:
+            continue
+        elif value == height_array[index - 1]:
+            return False
+        elif value > height_array[index - 1] and height_increase is False:
+            return False
+        else:
+            if value < height_array[index - 1]:
+                height_increase = False
+                continue
+    return True
 
 
 if __name__ == '__main__':
     with open('input.txt') as file_input:
-        a = int(file_input.readline())
-        b = int(file_input.readline())
-    result = main()
+        massive = file_input.readline().rstrip().rsplit(' ')
+
+    values = [int(value) for value in massive]
+    result = valid_mountain_array(values)
 
     with open('output.txt', 'w') as file_output:
         file_output.write(str(result))
