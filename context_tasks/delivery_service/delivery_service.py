f51bdbc03cb9445aff00ecb5b572ac36911a0278
Denis Yavorsky <svirgibs@gmail.com>
2024-07-21 19:31:26 +0300

    delivery_service.py

--- context_tasks/delivery_service/delivery_service.py
+++ context_tasks/delivery_service/delivery_service.py
@@ -1,16 +1,47 @@
-def get_min_transport_platforms(massive: list[int], limit: int) -> int:
-    pass
+def get_min_transport_platforms(values: list[int], limit: int) -> int:
+    count = total_weight = left_pointer = 0
+    values.sort()
+    right_pointer = len(values) - 1
+
+    if len(values) == 1:
+        count += 1
+        return count
+
+    while left_pointer <= right_pointer:
+        if (values[right_pointer] == limit or
+                values[right_pointer] + values[left_pointer] > limit):
+            count += 1
+            right_pointer -= 1
+            continue
+
+        if left_pointer == right_pointer:
+            count += 1
+            break
+
+        total_weight += values[left_pointer] + values[right_pointer]
+
+        if total_weight <= limit:
+            count += 1
+            total_weight = 0
+        elif total_weight > limit:
+            count += 2
+            total_weight = 0
+
+        left_pointer += 1
+        right_pointer -= 1
+
+    return count
 
 
 if __name__ == '__main__':
     with open('input.txt') as file_input:
-        massive = file_input.readline().rstrip()
+        massive = file_input.readline().rstrip().rsplit(' ')
         limit = file_input.readline().rstrip()
 
-    values = [int(value) for value in massive]
+    massive = [int(value) for value in massive]
     limit = int(limit)
 
-    result = get_min_transport_platforms(values, limit)
+    result = get_min_transport_platforms(massive, limit)
 
     with open('output.txt', 'w') as file_output:
         file_output.write(str(result))
