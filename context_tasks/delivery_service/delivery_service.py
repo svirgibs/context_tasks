37b526079cf7d94f9c7b26d14da8cef1cd7f3947
Denis Yavorsky <svirgibs@gmail.com>
2024-07-22 22:01:15 +0300

    delivery_service.py refactored

--- context_tasks/delivery_service/delivery_service.py
+++ context_tasks/delivery_service/delivery_service.py
@@ -2,33 +2,16 @@
 
 
 def get_min_transport_platforms(values: list[int], limit: int) -> int:
-    platform_count = total_weight = left_pointer = 0
+    platform_count = left_pointer = 0
     values.sort()
     right_pointer = len(values) - 1
 
     while left_pointer <= right_pointer:
-        if (values[right_pointer] == limit or
-                values[right_pointer] + values[left_pointer] > limit):
-            platform_count += 1
-            right_pointer -= 1
-            continue
-
-        if left_pointer == right_pointer:
-            platform_count += 1
-            break
-
-        total_weight += values[left_pointer] + values[right_pointer]
-
+        total_weight = values[left_pointer] + values[right_pointer]
         if total_weight <= limit:
-            platform_count += 1
-            total_weight = 0
-        elif total_weight > limit:
-            platform_count += 2
-            total_weight = 0
-
-        left_pointer += 1
+            left_pointer += 1
+        platform_count += 1
         right_pointer -= 1
-
     return platform_count
 
 
