b38b813d60a30460f237c921d0576481bc5ed113
Denis Yavorsky <svirgibs@gmail.com>
2024-07-28 12:52:16 +0300

    added

---
+++ context_tasks/counting_rhyme.py
@@ -0,0 +1,40 @@
+class Applicant:
+    def __init__(self, idx: int, checked: bool = False):
+        self.idx = idx,
+        self.checked = checked
+
+    def __repr__(self):
+        return repr(self.idx[0])
+
+
+def counting_rhyme(applicants_value: int, tacts_value: int):
+    applicants = [Applicant(x + 1) for x in range(applicants_value)]
+
+    while len(applicants) > 1:
+        tact = 1
+
+        for applicant in applicants:
+            if applicant.checked is True:
+                applicants.append(applicant)
+                applicant.checked = False
+                del applicants[0]
+
+        for index, applicant in enumerate(applicants):
+            applicant.checked = True
+            if tact == tacts_value:
+                applicants.pop(index)
+                break
+            tact += 1
+
+    return applicants[0].idx[0]
+
+
+if __name__ == '__main__':
+    with open('input.txt') as file_input:
+        applicants = int(file_input.readline().rstrip())
+        tacts = int(file_input.readline().rstrip())
+
+    result = counting_rhyme(applicants, tacts)
+    print(result)
+    with open('output.txt', 'w') as file_output:
+        file_output.write(str(result))
