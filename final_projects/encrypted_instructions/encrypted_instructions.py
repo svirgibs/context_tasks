cc5da887a2bb57111bc1564016903c14d1264e35
Denis Yavorsky <svirgibs@gmail.com>
2024-08-02 18:21:16 +0300

    не рабочая версия с циклом for

--- final_projects/encrypted_instructions/encrypted_instructions.py
+++ final_projects/encrypted_instructions/encrypted_instructions.py
@@ -1,9 +1,36 @@
 # Номер посылки -
 
-def get_decoding_instructions(encrypted_instructions: str) -> str:
+from string import digits, ascii_letters
+
+
+def get_decoding_instructions(encrypted: str) -> str:
     """Функция декодирует и возвращает инструкцию"""
     result = str()
-    
+    repeat = str()
+    repeat_in_bracket = str()
+    commands = str()
+    in_bracket = False
+    brackets = ['[', ']']
+
+    for char in encrypted:
+        if char in digits and not in_bracket:
+            repeat += char
+            continue
+        elif char in digits and in_bracket:
+            repeat_in_bracket += char
+        elif char in brackets:
+            if char == brackets[1]:
+                in_bracket = False
+                result += commands * int(repeat)
+                commands = str()
+                repeat = str()
+            else:
+                in_bracket = True
+        elif char in ascii_letters and in_bracket:
+            commands += char
+        elif char in ascii_letters and not in_bracket:
+            result += char
+
     return result
 
 
